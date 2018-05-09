0aa5d7d5f97c1e9cc6fbdaa6873bef21f72d0c26
Mark Coombes <mark@markcoombes.ca>
2018-05-08 22:13:46 -0400

    Update HomeSeer version (#2)

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ -e /DO_INSTALL ]
 then
     echo "HomeSeer (re)install/update required at container first run. Installing at /HomeSeer..."
 
-    wget -O /homeseer.tgz "http://homeseer.com/updates3/hs3_linux_$HOMESEER_VERSION.tar.gz"
+    wget -O /homeseer.tgz "http://homeseer.com/updates3/hslinux_hs3_$HOMESEER_VERSION.tar.gz"
     tar -xzo -C / -f /homeseer.tgz
     rm -f /homeseer.tgz
     rm -f /DO_INSTALL
